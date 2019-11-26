e820b66f2198ec6a0dbc6d5df11cef3c9a72d4f2
rbarakhvostov <rbarakhvostov@gmail.com>
2019-11-26 16:56:06 +0300

    book list refactoring

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -9,7 +9,23 @@ import { compose } from '../../utils';
 
 import './book-list.css';
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+  return (
+    <ul className='book-list'>
+      {
+        books.map((book) => {
+          return (
+            <li key={book.id}>
+              <BookListItem book={book} />
+            </li>
+          );
+        })
+      }
+    </ul>
+  );
+}
+
+class BookListContainer extends Component {
   componentDidMount() {
     this.props.fetchBooks();
   }
@@ -24,19 +40,7 @@ class BookList extends Component {
       return <ErrorIndicator />
     }
 
-    return (
-      <ul className='book-list'>
-        {
-          books.map((book) => {
-            return (
-              <li key={book.id}>
-                <BookListItem book={book} />
-              </li>
-            );
-          })
-        }
-      </ul>
-    );
+    return <BookList books={books} />
   }
 }
 
@@ -53,4 +57,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
