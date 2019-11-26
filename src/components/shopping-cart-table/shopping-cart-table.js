2d0262163419900d8c1164bda9916484f5bf42de
rbarakhvostov <rbarakhvostov@gmail.com>
2019-11-26 19:27:15 +0300

    connect shopping cart table to redux

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './shopping-cart-table.css';
 
-const ShoppingCartTable = () => {
+const ShoppingCartTable = ({ items, total, onIncrease, onDecrease, onDelete }) => {
+  const renderRow = (item, idx) => {
+    const { id, name, count, total } = item;
+    return (
+      <tr key={ id }>
+        <td>{ idx + 1 }</td>
+        <td>{ name }</td>
+        <td>{ count }</td>
+        <td>${ total }</td>
+        <td>
+          <button
+            onClick={() => onDecrease(id)}
+            className="btn btn-outline-warning btn-sm float-left">
+            <span className="fa fa-minus-circle"></span>
+          </button>
+          <button
+            onClick={() => onIncrease(id)}
+            className="btn btn-outline-success btn-sm float-left">
+            <span className="fa fa-plus-circle"></span>
+          </button>
+          <button
+            onClick={() => onDelete(id)}
+            className="btn btn-outline-danger btn-sm float-left">
+            <span className="fa fa-trash-o"></span>
+          </button>
+        </td>
+      </tr>
+    )
+  }
   return (
     <div className='shopping-cart-table'>
       <h2>Your Order</h2>
@@ -12,35 +41,42 @@ const ShoppingCartTable = () => {
             <th>#</th>
             <th>Item</th>
             <th>Count</th>
-            <th>Price</th>
+            <th>Total</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Eloquent JavaScript</td>
-            <td>2</td>
-            <td>$40</td>
-            <td>
-              <button className="btn btn-outline-danger btn-sm float-right">
-                <span className="fa fa-trash-o"></span>
-              </button>
-              <button className="btn btn-outline-success btn-sm float-right">
-                <span className="fa fa-plus-circle"></span>
-              </button>
-              <button className="btn btn-outline-warning btn-sm float-right">
-                <span className="fa fa-minus-circle"></span>
-              </button>
-            </td>
-          </tr>
+          {
+            items.map(renderRow)
+          }
         </tbody>
       </table>
       <div className='total'>
-        Total: $201
+        Total: ${ total }
       </div>
     </div>
   );
 }
 
-export default ShoppingCartTable;
+const mapStateToProps = ({ cartItems, orderTotal }) => {
+  return {
+    items: cartItems,
+    total: orderTotal,
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    onIncrease: (id) => {
+      console.log(`increase ${id}`)
+    },
+    onDecrease: (id) => {
+      console.log(`decrease ${id}`)
+    },
+    onDelete: (id) => {
+      console.log(`delete ${id}`)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTable);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,22 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [
+    {
+      id:1,
+      name: 'Book 1',
+      count: 3,
+      total: 150
+    },
+    {
+      id:2,
+      name: 'Book 2',
+      count: 2,
+      total: 220
+    }
+  ],
+  orderTotal: 220
 }
 
 const reducer = (state = initialState, action) => {
@@ -9,12 +24,14 @@ const reducer = (state = initialState, action) => {
   switch(action.type) {
     case 'FETCH_BOOKS_REQUEST':
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       }
     case 'FETCH_BOOKS_SUCCESS':
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
@@ -22,6 +39,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_BOOKS_FAILURE':
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
